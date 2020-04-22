6599b1528212b0f696fe4aa57956c501b658bd0f
Joseph Lindzius <josephlindzius@Josephs-MacBook-Pro.local>
2020-04-22 19:31:35 +0200

    Updates

---
+++ precache-manifest.8f676bd2ae0d453a809988e845e44fc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd91e67c591064001fe987b91bf84f71",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e4669bf69cba8262dcdc",
+    "url": "/react-portfolio/static/css/main.3d1dd216.chunk.css"
+  },
+  {
+    "revision": "03ccc23709f7e99336db",
+    "url": "/react-portfolio/static/js/2.38db7e38.chunk.js"
+  },
+  {
+    "revision": "6e900953f82e75bb7ccced89e6806918",
+    "url": "/react-portfolio/static/js/2.38db7e38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4669bf69cba8262dcdc",
+    "url": "/react-portfolio/static/js/main.4a52db8c.chunk.js"
+  },
+  {
+    "revision": "141e95f726defc8c0711",
+    "url": "/react-portfolio/static/js/runtime-main.922f30ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/LOGO.5d5d9eef.svg"
+  }
+]);
